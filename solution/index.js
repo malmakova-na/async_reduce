f6517de26c59227fa1289ee1d9dd5cf8bafbd954
malmakova.na <malmakova.na@phystech.edu>
2021-10-14 20:58:50 +0300

    add playground and solution

---
+++ solution/index.js
@@ -0,0 +1,40 @@
+/**
+ * type reduce = (
+    array: AsyncArray
+    fn: (
+        acc: any,
+        cur: any,
+        i: number,
+        src: AsyncArray,
+        cb: (result: any) => void
+    ) => void
+    initialValue: any
+    cb: (result: any) => void
+) => void
+ */
+module.exports = function (Homework) {
+
+    const getLength = async(arr) => new Promise((resolve) => arr.length((res)=>resolve(res)));
+    const getElement = async(arr, index) => new Promise((resolve) => arr.get(index, (res)=>resolve(res)));
+    const getAccumulator = async(acc, cur, i, src, fn) => new Promise((resolve) => {
+        return fn(acc, cur, i, src, (res)=> resolve(res));
+    });
+    const compare = async(a, b) => new Promise((resolve) => Homework.less(a, b, (res)=> resolve(res)));
+    const add = async(a,b) => new Promise((resolve) => Homework.add(a, b, (res)=> resolve(res)));
+    
+
+    return async (array, fn, initialValue, cb) => {
+        let answ = initialValue;
+        let i = 0;
+        let length = await getLength(array);
+        let isLess = await compare(i, length);
+        while(isLess){
+            let element = await getElement(array, i);
+            let result = await getAccumulator(answ, element, i, array,fn );
+            answ = result;
+            i = await add(i,1);
+            isLess = await compare(i, length)
+        }
+        return cb(answ);
+    }
+}
