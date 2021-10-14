49fbaa1d8f78a73cd9d16a8a8c8eee531d3ce58d
malmakova.na <malmakova.na@phystech.edu>
2021-10-14 21:17:17 +0300

    add updates

--- test/tests.js
+++ test/tests.js
@@ -4,13 +4,13 @@ const reduceFunction = require('../solution/index');
 const a = new Homework.AsyncArray([1, 2, 3, 4, 10]);
 const reduce = reduceFunction(Homework);
 
-const reducerSum = (acc, curr, i, src, cb) => Homework.add(acc, curr, cb);
-const reducerSub = (acc, curr, i, src, cb) => Homework.subtract(acc, curr, cb);
-const reducerMult = (acc, curr, i, src, cb) => Homework.multiply(acc, curr, cb);
-const reducerDiv = (acc, curr, i, src, cb) => Homework.divide(acc, curr, cb);
+const reducerAdd = (acc, curr, i, src, cb) => Homework.add(acc, curr, cb);
+const reducerSubstract = (acc, curr, i, src, cb) => Homework.subtract(acc, curr, cb);
+const reducerMultiply = (acc, curr, i, src, cb) => Homework.multiply(acc, curr, cb);
+const reducerDivide = (acc, curr, i, src, cb) => Homework.divide(acc, curr, cb);
 
 
-reduce(a, reducerSum, 0, (res)=> {console.log("reducerSum",res === 1 + 2 + 3 + 4+10)})
-reduce(a, reducerSub, 0, (res)=> {console.log("reducerSub",res===-1 - 2 - 3 - 4-10)})
-reduce(a, reducerMult, 1, (res)=> {console.log("reducerMult",res === (1 * 2 * 3 * 4*10))})
-reduce(a, reducerDiv, 1, (res)=> {console.log("reducerDiv",res===1 / 2 / 3 / 4/10)})
+reduce(a, reducerAdd, 0, (res)=> {console.log("reducerAdd test:",res === 1 + 2 + 3 + 4 + 10)});
+reduce(a, reducerSubstract, 0, (res)=> {console.log("reducerSubstract test:",res === -1 - 2 - 3 - 4 - 10)});
+reduce(a, reducerMultiply, 1, (res)=> {console.log("reducerMultiply test:",res === (1 * 2 * 3 * 4 * 10))});
+reduce(a, reducerDivide, 1, (res)=> {console.log("reducerDivide test:",res === 1 / 2 / 3 / 4 / 10)});
